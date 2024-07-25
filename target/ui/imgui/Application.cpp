e134198cace3d272aaa4ed1d50b90ec55644f67b
07Kevin <97pippifogeln@gmail.com>
2024-07-26 01:07:04 +0200

    fix

--- target/ui/imgui/Application.cpp
+++ target/ui/imgui/Application.cpp
@@ -123,8 +123,8 @@ int CApplication::Main()
    //IM_ASSERT(font != nullptr);
 
    // Our state
-   bool show_demo_window = true;
-   bool show_another_window = true;
+   bool bShowDemoWindow = true;
+   bool bShowAnotherWindow = true;
    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
    // Main loop
@@ -158,8 +158,8 @@ int CApplication::Main()
       ImGui::NewFrame();
 
       // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-      if(show_demo_window)
-         ImGui::ShowDemoWindow(&show_demo_window);
+      if(bShowDemoWindow)
+         ImGui::ShowDemoWindow(&bShowDemoWindow);
 
       // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
       {
@@ -169,8 +169,8 @@ int CApplication::Main()
          ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
          ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-         ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-         ImGui::Checkbox("Another Window", &show_another_window);
+         ImGui::Checkbox("Demo Window", &bShowDemoWindow);      // Edit bools storing our window open/close state
+         ImGui::Checkbox("Another Window", &bShowDemoWindow);
 
          ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
          ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
@@ -185,12 +185,12 @@ int CApplication::Main()
       }
 
       // 3. Show another simple window.
-      if(show_another_window)
+      if(bShowAnotherWindow)
       {
-         ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+         ImGui::Begin("Another Window", &bShowAnotherWindow);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
          ImGui::Text("Hello from another window!");
          if(ImGui::Button("Close Me"))
-            show_another_window = false;
+            bShowAnotherWindow = false;
          ImGui::End();
       }
 
